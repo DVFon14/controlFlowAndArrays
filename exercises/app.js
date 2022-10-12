edde741e181e24a83ef2ecbbbee119b8091e62b7
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-10-12 15:20:26 -0600

    Exercise 1 and 2

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,15 @@
 console.log("hello world");
+
+function test (num)
+{
+    if (num >0)
+    {console.log("positive")}
+    else if (num <0)
+    {console.log("negative")}
+    else if (num= 0)
+    {console.log("Neither positive nor negative")}
+    else
+    {console.log("That was not a number")}
+}
+
+test ();
